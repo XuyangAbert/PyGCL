12a161feff736997d1ea6d8f28a4c038b9f59239
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2025-03-12 23:40:47 -0500

    Update utils.py

--- GCL/utils.py
+++ GCL/utils.py
@@ -1,7 +1,7 @@
 from typing import *
 import os
 import torch
-import dgl
+# import dgl
 import random
 import numpy as np
 
@@ -57,9 +57,9 @@ def normalize(s):
     return (s.max() - s) / (s.max() - s.mean())
 
 
-def build_dgl_graph(edge_index: torch.Tensor) -> dgl.DGLGraph:
-    row, col = edge_index
-    return dgl.graph((row, col))
+# def build_dgl_graph(edge_index: torch.Tensor) -> dgl.DGLGraph:
+#     row, col = edge_index
+#     return dgl.graph((row, col))
 
 
 def batchify_dict(dicts: List[dict], aggr_func=lambda x: x):
